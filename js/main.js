a997d6397de05dd26d57a4ff2e620540a1ee6d23
ibag <bahniukihor@gmail.com>
2023-01-22 16:46:16 +0200

    2.0

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const buttons = document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button-option");
 
-const correct = document.querySelectorAll(".correct");
+const btnStart = document.querySelector("#start");
 
 const screens = document.querySelectorAll(".screen");
 
@@ -14,47 +14,46 @@ let trueCounter = 0;
 
 let falseCounter = 0;
 
+btnStart.addEventListener('click', () =>{
+    let currentScreen = btnStart.closest(".screen");
+    currentScreen.classList.remove("show");
+    let nextScreenIndex = screenArray.indexOf(currentScreen) + 1;
+    let nextScreen = screenArray[nextScreenIndex];
+    nextScreen.classList.add("show");
+})
+
 buttons.forEach(elem =>{
     elem.addEventListener('click', () =>{
-        if(!elem.classList.contains("start")){
-            if(elem.classList.contains("correct")){
+        let currentScreen = elem.closest(".screen");
+        let screenBtns = currentScreen.querySelectorAll(".button-option");
+        if(elem.classList.contains("correct")){
+            screenBtns.forEach(elem =>{
                 elem.disabled = true;
-                trueCounter += 1;
-                elem.classList.add("true");
-                elem.innerHTML = "True";
-                lastScreenResult.innerHTML = `You have ${trueCounter} correct answer(s)!`;
-            } else{
-                elem.disabled = true;
-                falseCounter += 1;
-                elem.classList.add("false");
-                elem.innerHTML = "False";
-            }
+            })
+            elem.disabled = true;
+            trueCounter++;
+            elem.classList.add("true");
+            elem.innerHTML = "True";
+            lastScreenResult.innerHTML = `You have ${trueCounter} correct answer(s)!`;
         }
-        if(!elem.classList.contains("start")){
-            setTimeout(() => {
-                let currentScreen = elem.closest(".screen");
-                currentScreen.classList.remove("show");
-                let nextScreenIndex = screenArray.indexOf(currentScreen) + 1;
-                let nextScreen = screenArray[nextScreenIndex];
-                nextScreen.classList.add("show");
-                if(screenArray.indexOf(currentScreen) === (screenArray.length - 1)){
-                    currentScreen.classList.remove("show");
-                    lastScreen.classList.add("show");
-                }
-              }, "1200") 
-        } else{
-            let currentScreen = elem.closest(".screen");
+        if(!elem.classList.contains("correct")){
+            screenBtns.forEach(elem =>{
+                elem.disabled = true;
+            })
+            elem.disabled = true;
+            falseCounter++;
+            elem.classList.add("false");
+            elem.innerHTML = "False";
+        }
+        setTimeout(() => {
             currentScreen.classList.remove("show");
             let nextScreenIndex = screenArray.indexOf(currentScreen) + 1;
             let nextScreen = screenArray[nextScreenIndex];
             nextScreen.classList.add("show");
-        } 
+            if(screenArray.indexOf(currentScreen) === (screenArray.length - 1)){
+                currentScreen.classList.remove("show");
+                lastScreen.classList.add("show");
+            }
+          }, "1200") 
     })
 })
-/*function ifTrue(elem) {
-    elem.disabled = true;
-    trueCounter += 1;
-    elem.classList.add("true");
-    elem.innerHTML = "True";
-    lastScreenResult.innerHTML = `You have ${trueCounter} correct answer(s)!`;
-} */
